454432678c7c15273f496294386b6029631381ab
fcarlone <frankcarlone@gmail.com>
2019-03-25 09:20:45 -0400

    Connect Firebase database to app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
-console.log('app.js')
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAYhkQjyM34uUO-T-NoBkKwxoh6QKvOV6w",
+  authDomain: "rps-online-game.firebaseapp.com",
+  databaseURL: "https://rps-online-game.firebaseio.com",
+  projectId: "rps-online-game",
+  storageBucket: "rps-online-game.appspot.com",
+  messagingSenderId: "462775286328"
+};
+firebase.initializeApp(config);
+
+let database = firebase.database();
