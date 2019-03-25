31065d3d271f808e8b435c3416298b7969c68ca6
fcarlone <frankcarlone@gmail.com>
2019-03-25 09:56:33 -0400

    Create function to get the number of connections to Firebase database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,4 +9,27 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// Firebase database reference
 let database = firebase.database();
+
+// Directory to store/track Player1 and Player2 connections
+let playerConnectionsRef = database.ref("/playerConnections");
+// Check player connection ".info/connected"
+let connectedRef = database.ref(".info/connected");
+
+// Player connection state changes
+connectedRef.on("value", function (snapshot) {
+  // If player is connected
+  if (snapshot.val()) {
+    // Add player to connected list
+    let con = playerConnectionsRef.push(true);
+    // Remove player from connection when they disconnect
+    con.onDisconnect().remove();
+  }
+});
+
+// On first load or change to connections list
+playerConnectionsRef.on("value", function (snapshot) {
+  // Get the number of players/children in the connections list and display on app
+  $("#connection-watch").text(`No. of connections ${snapshot.numChildren()}`)
+})
